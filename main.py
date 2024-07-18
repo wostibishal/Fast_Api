c06c9190f1f2117d9e836c9111e5a0aff9ede008
wostibishal <bishalwasti1@gmail.com>
2024-07-18 18:03:44 +0545

    FastAPI

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def index():
+    return {'data':{'name':'fast'}}
+
+@app.get('/about')
+def about():
+    return {'data':{'name':'Fasstapi'}}
