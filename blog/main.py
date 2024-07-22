257407b581a98c72953d574d7bbb216a0218a82e
wostibishal <bishalwasti1@gmail.com>
2024-07-22 07:40:15 +0545

    fast

--- blog/main.py
+++ blog/main.py
@@ -1,13 +1,24 @@
 from fastapi import Depends, FastAPI
 from .schemas import Blog
-from .models import  Base
-from .database import engine
+from . import  models
+from .database import engine, SessionLocal
 from sqlalchemy import Session
 
 app = FastAPI()
 
-Base.metadata.create_all(engine)
+models.Base.metadata.create_all(engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.post('/blog')
-def create(blog: Blog, db:Session = Depends(get_db)):
-    return db
+def create(request:Blog, db:Session = Depends(get_db)):
+    new_blog = models.Blog(title= request.title, body= request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
