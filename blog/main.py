5d7de6671735e0eb6eaf7dfb5379dd0cc9b0229d
wostibishal <bishalwasti1@gmail.com>
2024-07-22 08:02:31 +0545

    new

--- blog/main.py
+++ blog/main.py
@@ -2,7 +2,8 @@ from fastapi import Depends, FastAPI
 from .schemas import Blog
 from . import  models
 from .database import engine, SessionLocal
-from sqlalchemy import Session
+from sqlalchemy.orm import Session
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -15,6 +16,11 @@ def get_db():
     finally:
         db.close()
 
+
+@app.get("/")
+def read_root():
+    return{"message": "hello world"}
+
 @app.post('/blog')
 def create(request:Blog, db:Session = Depends(get_db)):
     new_blog = models.Blog(title= request.title, body= request.body)
@@ -22,3 +28,8 @@ def create(request:Blog, db:Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.get('/blog')
+def all(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
