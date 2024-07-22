453ec847aff69ff016798214bcf6c90222dc56cf
wostibishal <bishalwasti1@gmail.com>
2024-07-22 16:24:26 +0545

    ok

--- blog/main.py
+++ blog/main.py
@@ -45,11 +45,18 @@ def show(id, response:Response, db:Session = Depends(get_db)):
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id,request: Blog, db:Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).update(request)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"blog with id {id} not found")
+    blog.update(request)
+    db.commit()
     return blog
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db:Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
